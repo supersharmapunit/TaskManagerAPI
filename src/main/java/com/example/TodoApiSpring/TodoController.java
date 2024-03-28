5825847490d07ca4d254a1aaf9753bfc6238bc89
supersharmapunit <punit2000sharma@gmail.com>
2024-03-28 12:12:48 +0530

    added basic controller structure

---
+++ src/main/java/com/example/TodoApiSpring/TodoController.java
@@ -0,0 +1,18 @@
+package com.example.TodoApiSpring;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class TodoController {
+
+    public static List<Todo> todos;
+
+    public TodoController() {
+        todos = new ArrayList<>();
+        todos.add(new Todo(1, false, "Todo 1", 1));
+        todos.add(new Todo(2, true, "Todo 2", 2));
+    }
+}
